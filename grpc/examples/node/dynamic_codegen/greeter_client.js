0092c667d5a6c10ddc2db678a19209c3e0ac67ce
Sousa <jonatas.f.sousa@accenture.com>
2018-06-06 10:48:29 -0300

    tempos medidos com hrtime()

--- grpc/examples/node/dynamic_codegen/greeter_client.js
+++ grpc/examples/node/dynamic_codegen/greeter_client.js
@@ -24,31 +24,36 @@ var protoTest = grpc.load(PROTO_PATH).helloworld;
 function main() {
   for(ia = 0 ; ia < 20 ; ia++){
   var client = new protoTest.Greeter('localhost:50051', grpc.credentials.createInsecure());
+  const NS_PER_SEC = 1e9;
+  const MS_PER_SEC = 1e6;
+  let time;
+  let diff;
   //Cenário onde não passamos nenhum argumento para um método Void
-  console.log("************************")
-  // console.time('#VoidMetodoSemParametros')
-  const NS_PER_SEC = 1e6;
-  let time = process.hrtime();
+  console.log("************************************************************************************************")
+  time = process.hrtime();
   client.VoidMetodoSemParametros({}, function(err, response) {});
-  let diff = process.hrtime(time);
-  console.log(`Benchmark took ${diff[0] * NS_PER_SEC + diff[1]/NS_PER_SEC} milissegundos`);
-  // console.timeEnd('#VoidMetodoSemParametros')
+  diff = process.hrtime(time);
+  console.log(`#VoidMetodoSemParametros levou ${(diff[0] * NS_PER_SEC + diff[1])/MS_PER_SEC} milissegundos`);
 
 
 //Cenário onde passamos um Integer e recebemos um Integer
   let variavel;
   variavel = 5
   console.log("************************")
-  console.time('#EnviaIntRecebeInt')
+  time = process.hrtime();
   client.EnviaIntRecebeInt({name: variavel}, function(err, response) {});
-  console.timeEnd('#EnviaIntRecebeInt')
+  diff = process.hrtime(time);
+  console.log(`#EnviaIntRecebeInt levou ${(diff[0] * NS_PER_SEC + diff[1])/MS_PER_SEC} milissegundos`);
+
 
   //Cenário onde passamos um Long e recebemos um Long
   let variavelLong = 5000000000
   console.log("************************")
-  console.time('#EnviaLongRecebeLong')
+  time = process.hrtime();
   client.EnviaLongRecebeLong({message: variavelLong}, function(err, response) {});
-  console.timeEnd('#EnviaLongRecebeLong')
+  diff = process.hrtime(time);
+  console.log(`#EnviaLongRecebeLong levou ${(diff[0] * NS_PER_SEC + diff[1])/MS_PER_SEC} milissegundos`);
+
 
   //Cenário onde passamos 8 variáveis Long e recebemos um Long
   let variavelLong1 = 5000000000
@@ -60,11 +65,13 @@ function main() {
   let variavelLong7 = 5000000000
   let variavelLong8 = 5000000000
   console.log("************************")
-  console.time('#EnviaLong8RecebeLong')
+  time = process.hrtime();
   client.EnviaLong8RecebeLong({message1: variavelLong1, message2: variavelLong2, message3: variavelLong3,
                               message4: variavelLong4, message5: variavelLong5, message6: variavelLong6,
                               message7: variavelLong7, message8: variavelLong8}, function(err, response) {});
-  console.timeEnd('#EnviaLong8RecebeLong')
+  diff = process.hrtime(time);
+  console.log(`#EnviaLong8RecebeLong levou ${(diff[0] * NS_PER_SEC + diff[1])/MS_PER_SEC} milissegundos`);
+
 
  //Cenário onde passamos strings com valor de potências de 10 MUITO grandes e recebemos uma string com uma frase
   for(i = 10; i<=100; i= i+10){
@@ -72,10 +79,11 @@ function main() {
     console.log("************************")
     let print = 100-i;
     console.log("10^" + print)
-    console.time('#EnviaStringPotencia10RecebeString')
+    time = process.hrtime();
     client.EnviaStringPotencia10RecebeString({message: varString}, function(err, response) {
     });
-    console.timeEnd('#EnviaStringPotencia10RecebeString')
+    diff = process.hrtime(time);
+    console.log(`#EnviaStringPotencia10RecebeString levou ${(diff[0] * NS_PER_SEC + diff[1])/MS_PER_SEC} milissegundos`);
   }
 
   // let var1 = 'Alguma String'
@@ -83,10 +91,12 @@ function main() {
       message1: 'Alguma String',
       message2: "Alguma Outra String"
   };
-  console.time('#EnviaTipoComplexoRecebeTipoComplexo')
   console.log("************************")
+  time = process.hrtime();
   client.EnviaTipoComplexoRecebeTipoComplexo({messagem: varComplexo}, function(err, response) {});
-  console.timeEnd('#EnviaTipoComplexoRecebeTipoComplexo')
+  diff = process.hrtime(time);
+  console.log(`#EnviaTipoComplexoRecebeTipoComplexo levou ${(diff[0] * NS_PER_SEC + diff[1])/MS_PER_SEC} milissegundos`);
+
 
 
 function _criaStringPotenciaDe10(i){
